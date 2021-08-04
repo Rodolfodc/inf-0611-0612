115be85d55b20deca64c57cd05d0bdd985c3c5e6
Nicole Nogueira <nicole.nogueira.silva@gmail.com>
2021-08-03 21:27:55 -0300

    Ajustes na última questão.

--- inf0612/teste1a.R
+++ inf0612/teste1a.R
@@ -14,18 +14,22 @@ V <- c("Sabonete", "Detergente", "Refrigerante", "Carne", "Vinho", "Chocolate", 
 ## Perguntas:
 ## Quais os produtos que sao vendidos em Campinas, mas nao sao vendidos em Limeira?
 setdiff(C, L)
-# [1] "Xampu"        "Sabonete"     "Refrigerante" "Queijo"       "Suco"         "Vinho"
+# Os produtos vendidos em Campinas mas não em Limeira são: 
+# "Xampu"        "Sabonete"     "Refrigerante" "Queijo"       "Suco"         "Vinho"
 
 ## Quais os produtos que sao vendidos em Vinhedo, mas nao sao vendidos em Campinas?
 setdiff(V, C)
+# Os produtos vendidos em Vinhedo mas não em Vinhedo são: 
 # [1] "Detergente" "Carne"      "Papel"      "Iogurte"
 
 ## Quais os produtos que sao vendidos em pelo menos uma cidade?
 union(C, union(V, L))
+# Os produtos vendidos em pelo menos uma cidade são: 
 # [1] "Xampu"        "Sabonete"     "Arroz"        "Chocolate"    "Leite"        "Refrigerante" "Queijo"      
 # [8] "Suco"         "Vinho"        "Cerveja"      "Detergente"   "Carne"        "Papel"        "Iogurte" 
 
 ## Quais os produtos que sao vendidos em todas as cidades?
+# Os produtos vendidos em todas as cidades são: 
 intersect(V, intersect(C,L))
 # [1] "Chocolate" "Leite"  
 
@@ -33,3 +37,5 @@ intersect(V, intersect(C,L))
 ## possui todos os itens necessarios para atender a demanda de Limeira? 
 length(intersect(L, C)) == length(L)
 # [1] TRUE
+is.element(L, C)
+#Sim. Como podemos perceber, todos os produtos vendidos em Limeira também são vendidos em Campinas.
